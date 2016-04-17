cba02478bffaeff7bc417b30b54ee2d71d5ba0c8
Richa Chaturvedi <richachaturvedi@Richas-MacBook-Pro.local>
2016-04-17 16:23:55 -0400

    added dropdown menus in scatterplot

--- assets/js/scatterplot.js
+++ assets/js/scatterplot.js
@@ -21,7 +21,7 @@ var svg = d3.select("#school-visualization").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Initializes Variables
-var data,DATA,start,end, name;
+var data,start,end, name;
 var choice1 = "SAT_AVG_ALL";
 var choice2 = "C150_4";
 
@@ -49,10 +49,18 @@ var yAxis = d3.svg.axis()
 var xDraw = svg.append("g")
     .attr("class", "axis x-axis")
     .attr("transform", "translate(0," + height + ")");
+    //.append("text")
+    //.attr("x", 340)
+    //.attr("y", 50)
+    //.text("SAT SCORE");
 
 // add y axis
 var yDraw = svg.append("g")
     .attr("class", "axis y-axis");
+    //.append("text")
+    //.attr("x", 340)
+    //.attr("y", 50)
+    //.text("Fraction of students who graduate with 150% of expected time");
 
 // make the tooltip
 var tip = d3.tip()
@@ -131,6 +139,8 @@ function updateVisualization() {
     circle.enter()
         .append("circle");
 
+    // ORANGE IF PRIVATE
+    // PINK IF PUBLIC
     circle.transition()
         .duration(800)
         .attr("cx", function (d) { if(!isNaN(d[choice1]) && !isNaN(d[choice2])) {return x(d[choice1]);} })
@@ -143,22 +153,14 @@ function updateVisualization() {
         })
         .attr("r", 4);
 
-
     circle.on('mouseover', tip.show)
         .on('mouseout', tip.hide);
         //.on("click", showEdition);
-
 
     circle.exit()
         .transition()
         .duration(800)
         .remove();
-
-
-    //// append path
-    //path.transition()
-    //    .duration(800)
-    //    .attr("d", line(data));
 
     xDraw.transition()
         .duration(800)
